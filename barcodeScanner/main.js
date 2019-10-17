edb3b3818eb73bf74eac75e9e3627953e711541b
Frederic Blondeel <frederic.blondeel@student.kuleuven.be>
2019-10-17 19:30:41 +0200

    Html barcode + scanner

--- barcodeScanner/main.js
+++ barcodeScanner/main.js
@@ -18,72 +18,139 @@ imgElement.onload = function() {
   inputCanvas.height = imgElement.height;
   inputContext.drawImage(imgElement, 0, 0, imgElement.width, imgElement.height);
   inputImgData = inputContext.getImageData(0, 0, imgElement.width, imgElement.height);
-  var image = inputImgData.data;
-  // var image = rgbaToHsla(inputImgData.data);
-  scanner(image, inputImgData.width, inputImgData.height);
+  // var image = inputImgData.data;
+  var image = rgbaToHsla(inputImgData.data);
+  scanner(image, inputImgData.width, inputImgData.height, 15);
 };
 
-function scanner(image, width, height) {
+function scanner(image, width, height, sensitivity) {
   //Always searching on the middle line
   searchHeight = Math.round(height / 2);
+  scanned  = Array();
 
   for (var i = (searchHeight - 1) * width * 4; i < searchHeight * width  * 4; i += 4) {
+    // Hue color values + code: 180/1;288/2;336/3;24/4;72/5
     var H = image[i];
     var S = image[i + 1];
     var L = image[i + 2];
-    console.log(H, S, L);
+
+    // console.log(H, S, L);
+
+    if (H<180+sensitivity && H>180-sensitivity && !scanned.includes(1)){
+      scanned.push(1)
+    }
+    if (H<288+sensitivity && H>288-sensitivity && !scanned.includes(2)){
+      scanned.push(2)
+    }
+    if (H<336+sensitivity && H>336-sensitivity && !scanned.includes(3)){
+      scanned.push(3)
+    }
+    if (H<24+sensitivity && H>24-sensitivity && !scanned.includes(4)){
+      scanned.push(4)
+    }
+    if (H<72+sensitivity && H>72-sensitivity && !scanned.includes(5)){
+      scanned.push(5)
+    }
+    if (L<100-sensitivity && scanned.length === 5){
+      console.log("feest")
+      return scanned;
+    }
+
   }
+  console.log(scanned)
 
 }
-
 function rgbaToHsla(image) {
+
   for (let i = 0; i < image.length; i += 4) {
-      //convert rgb spectrum to 0-1
-      let red = image[i] / 255;
-      let green = image[i + 1] / 255;
-      let blue = image[i + 2] / 255;
-
-      let min = Math.min(red, green, blue);
-      let max = Math.max(red, green, blue);
-
-      let L = (min + max) / 2;
-      let S = findSaturation(min, max, L);
-      let H = findHue(red, green, blue, max, min);
-
-      image[i] = H;
-      image[i + 1] = Math.round(S * 100);
-      image[i + 2] = Math.round(L * 100);
+    //convert rgb spectrum to 0-1
+    let red = image[i] / 255;
+    let green = image[i + 1] / 255;
+    let blue = image[i + 2] / 255;
+    let min = Math.min(red, green, blue);
+    let max = Math.max(red, green, blue);
+    let L = (min + max) / 2;
+    image[i] = this.findHue(red, green, blue, max, min);
+    image[i + 1] = this.findSaturation(min, max, L)*100;
+    image[i + 2] = L*100;
   }
-
-  return image;
+  return image
 }
-
-function findSaturation(min, max, L) {
-  if (L < 0.5) {
-      return (max - min) / (max + min);
-  } else {
-      return (max - min) / (2.0 - max - min);
-  };
-}
-
 function findHue(red, green, blue, max, min) {
   let hue = 0;
-  if (max == min) {
-      return 0;
+  if (max === min) {
+    return 0;
   }
-  else if (red == max) {
-      hue = (green - blue) / (max - min);
+  else if (red === max) {
+    hue = ((green - blue) / (max - min))%6;
   }
-  else if (green == max) {
-      hue = 2.0 + (blue - red) / (max - min);
+  else if (green === max) {
+    hue = 2.0 + (blue - red) / (max - min);
   }
-  else if (blue == max) {
-      hue = 4.0 + (red - green) / (max - min);
+  else if (blue === max) {
+    hue = 4.0 + (red - green) / (max - min);
   }
-
-  hue *= 60;
-  if (hue < 0) {
-      hue += 360
-  }
-  return hue;
+  return hue*60;
 }
+function findSaturation(min, max, L) {
+  if (max-min === 0) {
+    return 0;
+  } else {
+    return (max - min) / (1.0 - Math.abs(2*L-1.0));
+  }
+}
+
+
+//old
+
+// function rgbaToHsla(image) {
+//   for (let i = 0; i < image.length; i += 4) {
+//       //convert rgb spectrum to 0-1
+//       let red = image[i] / 255;
+//       let green = image[i + 1] / 255;
+//       let blue = image[i + 2] / 255;
+//
+//       let min = Math.min(red, green, blue);
+//       let max = Math.max(red, green, blue);
+//
+//       let L = (min + max) / 2;
+//       let S = findSaturation(min, max, L);
+//       let H = findHue(red, green, blue, max, min);
+//
+//       image[i] = H;
+//       image[i + 1] = Math.round(S * 100);
+//       image[i + 2] = Math.round(L * 100);
+//   }
+//
+//   return image;
+// }
+//
+// function findSaturation(min, max, L) {
+//   if (L < 0.5) {
+//       return (max - min) / (max + min);
+//   } else {
+//       return (max - min) / (2.0 - max - min);
+//   };
+// }
+//
+// function findHue(red, green, blue, max, min) {
+//   let hue = 0;
+//   if (max == min) {
+//       return 0;
+//   }
+//   else if (red == max) {
+//       hue = (green - blue) / (max - min);
+//   }
+//   else if (green == max) {
+//       hue = 2.0 + (blue - red) / (max - min);
+//   }
+//   else if (blue == max) {
+//       hue = 4.0 + (red - green) / (max - min);
+//   }
+//
+//   hue *= 60;
+//   if (hue < 0) {
+//       hue += 360
+//   }
+//   return hue;
+// }
