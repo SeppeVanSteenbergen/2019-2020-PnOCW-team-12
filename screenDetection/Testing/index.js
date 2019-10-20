0aa570b08757b554d665e052f0d94d16586f8fcd
martijndebeuf <martijn.debeuf@student.kuleuven.be>
2019-10-20 16:07:01 +0200

    server serving the test images

--- screenDetection/Testing/index.js
+++ screenDetection/Testing/index.js
@@ -1,19 +1,59 @@
-const http = require('http');
-const server = http.createServer();
-const  fs = require('fs');
+/*
+    code used from: https://www.codexpedia.com/node-js/node-js-http-server-displaying-images-from-a-directory/
+ */
 
-fs.readFile('public/index.html', function (err, html) {
-    if (err) {
-        throw err;
+//include http, fs and url module
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    url = require('url');
+imageDir = 'public/Images/';
+
+//create http server listening on port 3333
+http.createServer(function (req, res) {
+    //use the url to parse the requested url and get the image name
+    var query = url.parse(req.url,true).query;
+    var pic = query.image;
+
+    if (typeof pic === 'undefined') {
+        getImages(imageDir, function (err, files) {
+            var imageLists = '<ul>';
+            for (var i=0; i<files.length; i++) {
+                imageLists += '<li><a href="/?image=' + files[i] + '">' + files[i] + '</li>';
+            }
+            imageLists += '</ul>';
+            res.writeHead(200, {'Content-type':'text/html'});
+            res.end(imageLists);
+        });
+    } else {
+        //read the image using fs and send the image content back in the response
+        fs.readFile(imageDir + pic, function (err, content) {
+            if (err) {
+                res.writeHead(400, {'Content-type':'text/html'})
+                console.log(err);
+                res.end("No such image", imageDir + pic);
+            } else {
+                //specify the content type in the response will be an image
+                res.writeHead(200,{'Content-type':'image/jpg'});
+                res.end(content);
+            }
+        });
     }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8080);
-    console.log("listening on port: 8080");
-});
 
-server.on('connection',socket => {
-    console.log("new connection");
-});
+}).listen(8080);
+
+console.log("Server running at http://localhost:8080/");
+
+//get the list of jpg files in the image dir
+function getImages(imageDir, callback) {
+    var fileType = '.jpg',
+        files = [], i;
+    fs.readdir(imageDir, function (err, list) {
+        for(i=0; i<list.length; i++) {
+            if(path.extname(list[i]) === fileType) {
+                files.push(list[i]); //store the file name into the array files
+            }
+        }
+        callback(err, files);
+    });
+}
