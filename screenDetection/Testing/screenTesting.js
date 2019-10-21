4eb3df272b1f57eb30fa8579beb84e37b005a71d
toonsauvillers <toon.sauvillers@gmail.com>
2019-10-21 12:55:30 +0200

    added image list for testing

--- screenDetection/Testing/screenTesting.js
+++ screenDetection/Testing/screenTesting.js
@@ -1,22 +1,37 @@
 const fs = require('fs');
 const sourceFolder = "./Images/";
 
-var images = [];
+/*
+['image name', nb screens, [screen size], [orientation screen]]
+*/
+var images = [
+    ['orientation1', 1, [400000], [0]], //0
+    ['orientation2', 1, [400000], [90]],
+    ['orientation3', 1, [400000], [180]],
+    ['orientation4', 1, [400000], [270]],
+    ['orientation5', 1, [400000], [45]],
+    ['orientation6', 1, [400000], [6.5]],
+    ['orientation7', 1, [400000], [347]], // 6
+    ['size1', 1, [26000], [0]], // 7
+    ['size2', 1, [140000], [0]],
+];
 
 
 
-
-
-function loadImages() {
+function loadImage(imageName) {
     fs.readdir(sourceFolder, (err, files) => {
         if(err) {console.error("Could not load the images")}
 
         files.forEach(file => {
-            if(file.includes("orientation"))
-                images.push(file);
+            if(file.includes(imageName))
+                return file;
         });
         console.log(images);
     });
 }
 
-loadImages();
+function load(){
+    for(let i = 0; i < images.length; i++){
+        this.images[i] = loadImage(this.images[i][0]);
+    }
+}
