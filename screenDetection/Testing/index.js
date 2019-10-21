3d865953b4ef8a86141c7ed8f01fca08883ccec6
martijndebeuf <martijn.debeuf@student.kuleuven.be>
2019-10-21 14:11:38 +0200

    Revert "Auto stash before merge of "feature/screenDetection" and "origin/feature/screenDetection""
    This reverts commit 8f84350ae3a7635e0da42430691019598be501dd.

--- screenDetection/Testing/index.js
+++ screenDetection/Testing/index.js
@@ -3,7 +3,7 @@
  */
 
 //include http, fs and url module
-const http = require('http'),
+var http = require('http'),
     fs = require('fs'),
     path = require('path'),
     url = require('url');
@@ -12,19 +12,18 @@ imageDir = 'public/Images/';
 //create http server listening on port 3333
 http.createServer(function (req, res) {
     //use the url to parse the requested url and get the image name
-    let query = url.parse(req.url,true).query;
-    let pic = query.image;
+    var query = url.parse(req.url,true).query;
+    var pic = query.image;
 
     if (typeof pic === 'undefined') {
         getImages(imageDir, function (err, files) {
-            let script = '<script src="screenTesting.js" type="text/javascript"> console.log("helloworld")</script>';
-            let imageLists = '<ul>';
-            for (let i=0; i<files.length; i++) {
+            var imageLists = '<ul>';
+            for (var i=0; i<files.length; i++) {
                 imageLists += '<li><a href="/?image=' + files[i] + '">' + files[i] + '</li>';
             }
             imageLists += '</ul>';
             res.writeHead(200, {'Content-type':'text/html'});
-            res.end(imageLists + script);
+            res.end(imageLists);
         });
     } else {
         //read the image using fs and send the image content back in the response
@@ -47,7 +46,7 @@ console.log("Server running at http://localhost:8080/");
 
 //get the list of jpg files in the image dir
 function getImages(imageDir, callback) {
-    let fileType = '.jpg',
+    var fileType = '.jpg',
         files = [], i;
     fs.readdir(imageDir, function (err, list) {
         for(i=0; i<list.length; i++) {
@@ -58,6 +57,3 @@ function getImages(imageDir, callback) {
         callback(err, files);
     });
 }
-
-//const formBegin = '<!doctype html> \ <html lang="en"> \ <head> \ <meta charset="UTF-8"> \ <title>ScreenDetectionTest</title> \ </head>  \ <body> ';
-//const formEnd = '</body> \ </html>';
