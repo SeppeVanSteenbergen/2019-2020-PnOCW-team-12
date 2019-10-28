e67bc66fd50d0157260dfe2e6c92ff247663979a
toonsauvillers <toon.sauvillers@gmail.com>
2019-10-28 18:26:48 +0100

    cry

--- delaunayTriangulation/secondAttempt/delaunayTriangulation.js
+++ delaunayTriangulation/secondAttempt/delaunayTriangulation.js
@@ -33,11 +33,50 @@ function delaunay(points){
     for(let i = 0; i < points.length; i++){
         let badTriangles = []
         for(let tri = 0; tri < triangulation.length; tri++){
-           // if()
+            let radius = calcRadius(triangulation[tri].points[0], triangulation[tri].points[1], triangulation[tri].points[2])
+            let center = calcCenter(triangulation[tri].points[0], triangulation[tri].points[1], triangulation[tri].points[2])
+           if((points[i].x - center.x)^2 + (points[i].y - center.y)^2 < radius^2){
+               badTriangles.push(triangulation[tri])
+           }
+        }
+        let polygon = new Set()
+        for(let tri = 0; tri < badTriangles.length; tri++){
+            for(let edg = 0; edg < badTriangles[tri].edges.length; edg++){
+                //zie pseudocode op wikipedia
+            }
         }
     }
 
 }
+//math for finding center of 3 points from paulbourke.net/geometry/circlesphere
+function calcCenter(point1, point2, point3){
+    let ma = (point2[1] - point1[1]) / (point2[0] - point1[0])
+    let mb = (minPoint[1] - point2[1]) / (minPoint[0] - point2[0])
+    let counter = 3
+    while(counter > 0 && (!isFinite(ma) || ! isFinite(mb) || ma == mb)){
+        let helpPoint = point1.slice(0)
+        point1 = point2.slice(0)
+        point2 = point3.slice(0)
+        point3 = helpPoint
+        ma = (point2[1] - point1[1]) / (point2[0] - point1[0])
+        mb = (point3[1] - point2[1]) / (point3[0] - point2[0])
+        counter--
+    }
+    let centerX = (ma * mb * (point1[1] - point3[1]) + mb * (point1[0] + point2[0]) - ma * (point2[0] + point3[0])) / (2* (mb - ma))
+    let centerY = -(centerX - (point1[0] + point2[0]) / 2) / ma + (point1[1] + point2[1]) / 2
+    return [centerX, centerY]
+}
+
+function calcRadius(point1, point2, point3){
+    // console.log("3 punten", point1, point2, point3)
+    let dist1 = calcDistance(point2, point3)
+    let dist2 = calcDistance(point1, point3)
+    let dist3 = calcDistance(point1, point2)
+    let numerator = dist1 * dist2 * dist3
+    let denumerator = Math.sqrt((dist1 + dist2 + dist3) * (dist2 + dist3 - dist1) * (dist3 + dist1 - dist2) * (dist1 + dist2 - dist3))
+    return numerator / denumerator
+}
+
 /*
     calculates crossproduct between two points, point3 as basis
 */
