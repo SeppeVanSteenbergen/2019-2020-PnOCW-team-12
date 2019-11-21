973c8a20b590dec6bbbeda960fe9beaf264bd1ad
Unknown <seppevansteenbergen@hotmail.com>
2019-11-21 17:16:57 +0100

    Same as Toon but in screenDetection

--- Testing/pub2/screenDetectionTest.js
+++ Testing/pub2/screenDetectionTest.js
@@ -9,13 +9,6 @@ inputElement.addEventListener(
   false
 );
 imgElement.onload = function() {
-  // Resize to max full HD resolution
-  let maxAmountBorderPx = 1920 + 1080;
-  if (imgElement.width + imgElement.height > maxAmountBorderPx) {
-    let ratio = imgElement.width / imgElement.height;
-    imgElement.height = Math.round(maxAmountBorderPx / (ratio + 1.0));
-    imgElement.width = Math.round(ratio * imgElement.height);
-  }
   let inputCanvas = document.getElementById('inputImage');
   let inputContext = inputCanvas.getContext('2d');
   inputContext.clearRect(0, 0, inputCanvas.width, inputCanvas.height);
@@ -32,8 +25,6 @@ imgElement.onload = function() {
     inputImgData,
     'imageOutConcatenated',
     'RGBA',
-    imgElement.width,
-    imgElement.height,
     null
   );  
   ColorSpace.hslaToRgba(imageTest.pixels);
