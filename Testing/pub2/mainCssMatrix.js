773daf7114cd3d18babb7e5eaf7efbfb92299720
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2019-11-24 19:08:45 +0100

    fixed css matrix test page

--- Testing/pub2/mainCssMatrix.js
+++ Testing/pub2/mainCssMatrix.js
@@ -3,39 +3,67 @@ var screen = {
   height: 1080
 }
 
-let imgElement = document.createElement('img');
-imgElement = document.getElementById('imageSrc');
-let inputElement = document.getElementById('fileInput');
+let imgElement = document.createElement('img')
+imgElement = document.getElementById('imageSrc')
+let inputElement = document.getElementById('fileInput')
 inputElement.addEventListener(
   'change',
   e => {
-    imgElement.src = URL.createObjectURL(e.target.files[0]);
+    imgElement.src = URL.createObjectURL(e.target.files[0])
   },
   false
-);
-imgElement.onload = function () {
-  let resizedImage = Image.resizeImage(imgElement, [1920, 1080]);
-  let inputCanvas = document.getElementById('inputImage');
-  let inputContext = inputCanvas.getContext('2d');
-  inputContext.clearRect(0, 0, inputCanvas.width, inputCanvas.height);
-  inputCanvas.width = resizedImage.width;
-  inputCanvas.height = resizedImage.height;
-  inputContext.drawImage(resizedImage, 0, 0, resizedImage.width, resizedImage.height);
-  let inputImgData = inputContext.getImageData(0, 0, resizedImage.width, resizedImage.height);
-  let imageTest = new Image(inputImgData, 'inputImage', 'RGBA',
-    [{ size: { width: screen.width, height: screen.height } }, { size: { width: screen.width, height: screen.height } }, { size: { width: screen.width, height: screen.height } }]
-  );
+)
+imgElement.onload = function() {
+  //let resizedImage = Image.resizeImage(imgElement, [1920, 1080]);
+  let c = document.createElement('canvas')
+  c.width = imgElement.width
+  c.height = imgElement.height
+  let ctx = c.getContext('2d')
+  ctx.drawImage(imgElement, 0, 0, c.width, c.height)
 
-  let transImage = document.createElement('img');
-  transImage = document.getElementById('transImg');
-  let [boxWidth, boxHeight] = imageTest.createPictureCanvas(transImage.width, transImage.height)
+  let imgData = ctx.getImageData(0, 0, c.width, c.height)
+  let imgDataResized = Image.resizeImageData(imgData, [1920, 1080])
+
+  let inputCanvas = document.getElementById('inputImage')
+  let inputContext = inputCanvas.getContext('2d')
+  inputContext.clearRect(0, 0, inputCanvas.width, inputCanvas.height)
+  inputCanvas.width = imgDataResized.width
+  inputCanvas.height = imgDataResized.height
+  inputContext.putImageData(imgDataResized, 0, 0)
+  let imageTest = new Image(imgDataResized, 'inputImage', 'RGBA', [
+    { size: { width: screen.width, height: screen.height } },
+    { size: { width: screen.width, height: screen.height } },
+    { size: { width: screen.width, height: screen.height } }
+  ])
+
+  let transImage = document.createElement('img')
+  transImage = document.getElementById('transImg')
+  let [boxWidth, boxHeight] = imageTest.createPictureCanvas(
+    transImage.width,
+    transImage.height
+  )
 
   let extremeValues = imageTest.findExtremeValues()
 
   let h = imageTest.screens[0].cssMatrix
   // let h2 = imageTest.screens[1].cssMatrix
   // let h3 = imageTest.screens[2].cssMatrix
-  let t = "position: absolute; left:" + extremeValues.minx + "px; top: " + extremeValues.miny + "px; transform: matrix3d(" + h.join(", ") + "); transform-origin: " + -extremeValues.minx + "px " + -extremeValues.miny + "px; width: " + boxWidth + "px; height: " + boxHeight + "px; object-fit: none";
+  let t =
+    'position: absolute; left:' +
+    extremeValues.minx +
+    'px; top: ' +
+    extremeValues.miny +
+    'px; transform: matrix3d(' +
+    h.join(', ') +
+    '); transform-origin: ' +
+    -extremeValues.minx +
+    'px ' +
+    -extremeValues.miny +
+    'px; width: ' +
+    boxWidth +
+    'px; height: ' +
+    boxHeight +
+    'px; object-fit: none'
   // let t2 = "position: absolute; left:" + extremeValues.minx + "px; top: " + extremeValues.miny + "px; transform: matrix3d(" + h2.join(", ") + "); transform-origin: " + -extremeValues.minx + "px " + -extremeValues.miny + "px; width: " + boxWidth + "px; height: " + boxHeight + "px; object-fit: none";
   //let t3 = "position: absolute; left:" + extremeValues.minx + "px; top: " + extremeValues.miny + "px; transform: matrix3d(" + h3.join(", ") + "); transform-origin: " + -extremeValues.minx + "px " + -extremeValues.miny + "px; width: " + boxWidth + "px; height: " + boxHeight + "px; object-fit: none";
 
@@ -44,7 +72,13 @@ imgElement.onload = function () {
   let outputContext = outputCanvas.getContext('2d')
   outputCanvas.width = boxWidth
   outputCanvas.height = boxHeight
-  outputContext.drawImage(transImage, 0, 0, outputCanvas.width, outputCanvas.height)
+  outputContext.drawImage(
+    transImage,
+    0,
+    0,
+    outputCanvas.width,
+    outputCanvas.height
+  )
 
   // let outputCanvas2 = document.getElementById("output2")
   // outputCanvas2.style = t2
@@ -60,6 +94,6 @@ imgElement.onload = function () {
   // outputCanvas3.height = boxHeight
   // outputContext3.drawImage(transImage, 0, 0, outputCanvas3.width, outputCanvas3.height)
 
-  ColorSpace.hslaToRgba(imageTest.pixels);
-  imageTest.show();
-};
+  ColorSpace.hslaToRgba(imageTest.pixels)
+  imageTest.show()
+}
