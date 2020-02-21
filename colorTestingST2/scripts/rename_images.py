8012df8aae248c514492f5437059174839c782fc
Unknown <seppevansteenbergen@hotmail.com>
2020-02-21 16:25:09 +0100

    Naming scripts

--- colorTestingST2/scripts/rename_images.py
+++ colorTestingST2/scripts/rename_images.py
@@ -1,22 +1,26 @@
 import os
 import glob
 
-dirs = [name for name in os.listdir(os.getcwd()) if os.path.isdir(name)]
-for dir in dirs:
-    path = os.path.join(os.getcwd(), dir)
-    images = [file for file in glob.glob(os.path.join(path, "**/*.txt"), recursive=True)]
-    names = [image.split("/")[-1] for image in images]
-    used_names = [name for name in names if dir in name]
-    old_names = [name for name in names if dir not in name]
+props = [prop for prop in os.listdir(os.getcwd()) if os.path.isdir(prop)]
+for prop in props:
 
-    for old_name in old_names:
-        i = 0
-        new_name = dir + "_" + str(i) + ".txt"
+    colors = os.listdir(os.path.join(os.getcwd(), prop))
+    for color in colors:
+        path = os.path.join(os.getcwd(), prop, color)
 
-        while new_name in used_names:
-            i += 1
-            new_name = dir + "_" + str(i) + ".txt"
+        images = [file for file in glob.glob(os.path.join(path, "**/*.jpg"), recursive=True)]
+        names = [image.split("/")[-1] for image in images]
+        used_names = [name for name in names if prop and color in name]
+        old_names = [name for name in names if prop and color not in name]
 
-        used_names.append(new_name)
+        for old_name in old_names:
+            i = 0
+            new_name = prop + "_" + color + "_" + str(i) + ".jpg"
 
-        os.rename(os.path.join(path, old_name), os.path.join(path, new_name))
+            while new_name in used_names:
+                i += 1
+                new_name = prop + "_" + color + "_" + str(i) + ".jpg"
+
+            used_names.append(new_name)
+
+            os.rename(os.path.join(path, old_name), os.path.join(path, new_name))
