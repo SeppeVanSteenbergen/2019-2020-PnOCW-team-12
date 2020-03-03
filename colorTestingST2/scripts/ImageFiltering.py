18501cab55c4d4d52a6622996c55659569d2b266
Unknown <seppevansteenbergen@hotmail.com>
2020-03-03 13:46:20 +0100

    Cropped image

--- colorTestingST2/scripts/ImageFiltering.py
+++ colorTestingST2/scripts/ImageFiltering.py
@@ -24,15 +24,20 @@ def rgb_to_hsl(r, g, b):
 
 def prep_image(file):
     image = cv2.imread(file)
-    b, g, r = cv2.split(image)
+    row, column, channels = image.shape
+    top_left = [int(row * (2 / 5)), int(column * (2 / 5))]
+    bottom_right = [int(row * (3 / 5)), int(column * (3 / 5))]
+    cropped_image = image[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1], :]
 
-    return np.concatenate(r), np.concatenate(g), np.concatenate(b)
+    b, g, r = cv2.split(cropped_image)
+
+    return r, g, b
 
 
 path = os.getcwd()
+
 files = [file for file in glob.glob(os.path.join(path, "**/*.jpg"), recursive=True)]
 for file in files:
     R, G, B = prep_image(file)
-    print(R, G, B)
-    H, S, L = rgb_to_hsl(R, G, B)
-    print(H, S, L)
+    # H, S, L = rgb_to_hsl(R, G, B)
+    # print(H, S, L)
