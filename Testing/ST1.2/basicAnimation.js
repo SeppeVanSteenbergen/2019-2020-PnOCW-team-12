e62f61f09389367b962a1b6922fdb213b563c011
Bert Van den Bosch <bert@fotovdb.com>
2020-03-08 20:24:49 +0100

    Browser timer drift

--- Testing/ST1.2/basicAnimation.js
+++ Testing/ST1.2/basicAnimation.js
@@ -39,9 +39,10 @@ var position2 = 0;
 var timeoutBox = document.getElementById("timeout-box")
 const FREQ = 16.6; //frame speed (16.6ms voor 60fps)
 
-function moveBox(){
+function moveBox() {
   timeoutBox.style.transform = 'translateX(' + Math.min(position2, 400) + 'px)';
   position2 += stepSize;
+  timeoutBox.style.width = '100px';
 
   // sleep( 30)
 
@@ -51,21 +52,21 @@ function moveBox(){
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-function animateTextTimeout(){
+function animateTextTimeout() {
   // timeoutBox.innerHTML = nextLetter()
 
   timeoutBox.style.transform = 'translateX(' + Math.min(position2, 400) + 'px)';
   position2 += stepSize;
 
   timeoutBox.innerHTML = nextLetter();
-  
+
   // sleep(15)
 
   setTimeout(animateTextTimeout, FREQ)
 }
 
 var letterIndex = 0;
-function nextLetter(){
+function nextLetter() {
 
   letterIndex++;
 
@@ -77,10 +78,55 @@ function nextLetter(){
 
 }
 
+function animateLayout() {
+  var w1 = document.getElementById("request-box").style.width;
+  document.getElementById("request-box").style.width = w1 + 10 + 'px';
+
+  var w2 = document.getElementById("timeout-box").style.width;
+  document.getElementById("timeout-box").style.width = w2 + 10 + 'px';
+
+  setTimeout(animateLayout, FREQ)
+}
+
+var drifts = [];
+//modified drift test from https://github.com/nodejs/node/issues/21822  
+function testDrift() {
+  const start = Date.now();
+  var counter = 1;
+  setInterval(() => {
+    var driftT = Date.now() - start - (counter * 1000);
+
+    // do some work
+    for (var i = 0; i < 100000; i++) {
+      var d = new Date();
+    }
+
+    timeoutBox.style.transform = 'translateX(' + Math.min(position2, 400) + 'px)';
+    position2 += stepSize;
+
+    // log the drift from the start (this should be fluctuating around ~0)
+    // you can run this in chrome, edge, etc (it does keep drifting in firefox)
+    // in nodejs it keeps growing
+    // console.log(`STEP: ${counter}, Drift: ${driftT}`);
+    // console.log(driftT)
+
+    drifts.push(driftT)
+
+    if(drifts.length == 20){
+      console.log(drifts)
+    }
+
+    counter++;
+  }, 1000);
+}
+
 
 // initial start of animations
 // moveBox();
 // window.requestAnimationFrame(step);
 // moveBox()
 
-animateTextTimeout()
+// animateTextTimeout()
+// animateLayout()
+
+testDrift()
