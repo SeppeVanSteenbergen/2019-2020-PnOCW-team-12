cd20ae5b170d32e9f391753afc5ba5bbadb21306
Unknown <seppevansteenbergen@hotmail.com>
2020-03-21 18:30:14 +0100

    New cahnges

--- TestingSem2/barcode/Iterator.js
+++ TestingSem2/barcode/Iterator.js
@@ -8,8 +8,8 @@ class Iterator {
             this.rightPoint = LU
           }
         
-        this.width = width
-        this.height = height
+        this.width = width--
+        this.height = height--
 
         this.a = (this.rightPoint[1] - this.leftPoint[1]) /
         (this.rightPoint[0] - this.leftPoint[0])
@@ -39,18 +39,16 @@ class Iterator {
             }
             this.b = this.y - this.a * this.x
             
-            if (this.y >= this.height) {
-                this.y = this.height -1
-                if (this.a > 0) {
-
+            if (this.y > this.height || this.y < 0) {
+                this.x = Math.round((this.y - this.b) / this.a)
+                this.y = this.height
                 }
                 this.isTerminated = true
-            }
 
         } else {
             this.y = Math.round(this.a * this.x + this.b)
 
-            if (this.y >= this.height || this.y < 0 || this.x >= this.width) {
+            if (this.y > this.height || this.y < 0 || this.x > this.width) {
                 this.x = -1
                 this.y = Math.round(this.b)
                 this.next()
