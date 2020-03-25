c9cb8916eb32bf3982da5d7ce0d820e3060493f7
Unknown <seppevansteenbergen@hotmail.com>
2020-03-25 13:20:33 +0100

    added nextRow to iterator

--- TestingSem2/barcode/PixelIterator.js
+++ TestingSem2/barcode/PixelIterator.js
@@ -1,25 +1,21 @@
 class PixelIterator {
   constructor(LU, RU, width, height) {
-    if (LU[0] <= RU[0]) {
-      this.leftPoint = LU
-      this.rightPoint = RU
+    this.line = new Line([LU[0], -LU[1]], [RU[0], -RU[1]])
+
+    this.horizontalMode = this.line.normalAngle <= 45 || (this.line.normalAngle >= 135 && this.line.normalAngle <= 225) || this.line.normalAngle >= 315
+    this.reversed = this.horizontalMode ? this.line.normalAngle > 45 && this.line.normalAngle < 315 : this.line.normalAngle < 225
+
+    this.width = width
+    this.height = height
+
+    if (this.horizontalMode) {
+      this.b = -LU[1] - this.line.slope * LU[0] + 1
+      this.x = 0
     } else {
-      this.leftPoint = RU
-      this.rightPoint = LU
+      this.b = -LU[1] - this.line.slope * LU[0]
+      this.intersectionXAxis = Math.round(-this.b / this.line.slope) - 1
+      this.y = 0
     }
-
-    this.width = width - 1
-    this.height = height - 1
-
-    this.a = -(this.rightPoint[1] - this.leftPoint[1]) /
-        (this.rightPoint[0] - this.leftPoint[0])
-
-    if (this.a >= 0) {
-      this.y = -1
-    } else {
-      this.y = this.height + 1
-    }
-    this.x = -1
 
     this.isTerminated = false
   }
@@ -63,5 +59,72 @@ class PixelIterator {
       }
     }
     return [this.x, this.y]
+  }
+
+  hasNextRow() {
+    return !this.isTerminated
+  }
+
+  nextRow() {
+    let row = []
+    if (this.horizontalMode) {
+      this.b--
+      this.y = Math.round(this.line.slope * this.x + this.b)
+      console.log(this.b)
+
+      while (this.x < this.width) {
+        if (this.y <= 0 && -this.y < this.height) {
+          row.push([this.x, -this.y])
+        }
+        this.x++
+        this.y = Math.round(this.line.slope * this.x + this.b)
+      }
+
+      this.x--
+      this.y = Math.round(this.line.slope * this.x + this.b)
+      if (-this.y >= this.height - 1 && -this.b >= this.height - 1) {
+        this.isTerminated = true
+      }
+
+      this.x = 0
+    } else {
+      this.intersectionXAxis ++
+      this.b = Math.round(-this.line.slope * this.intersectionXAxis)
+      console.log(this.b)
+
+      while (this.x >= 0 || -this.y < this.height) {
+        if (-this.y < this.height) {
+          row.push([this.x, -this.y])
+
+        }
+
+        this.y--
+        this.x = Math.round((this.y - this.b) / this.line.slope)
+      }
+
+      this.y++
+      this.x = Math.round((this.y - this.b) / this.line.slope)
+
+
+
+      if (this.b < -100) {
+        this.isTerminated = true
+      }
+
+      this.x = Math.round(-this.b / this.line.slope) - 1
+      this.y = 0
+    }
+
+    if (this.reversed) {
+      row = row.reverse()
+    }
+
+    return row
+  }
+
+  test() {
+    while (this.hasNextRow()) {
+      console.log(this.nextRow())
+    }
   }
 }
