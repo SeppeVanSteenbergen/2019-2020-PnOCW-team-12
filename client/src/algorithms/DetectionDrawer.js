1b02888a3f4c47543caec22966ee6be795131ded
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2020-04-01 13:20:02 +0200

    new DetectionDrawer

--- client/src/algorithms/DetectionDrawer.js
+++ client/src/algorithms/DetectionDrawer.js
@@ -5,136 +5,157 @@ export default class DetectionDrawer {
     this.w = screen.width
     this.h = screen.height
     this.borderWidth = borderWidth
-    this.c1 = '#0000ff'
-    this.c2 = '#00ff00'
-    this.white = '#ffffff'
+    this.c1 = '#ffff00'
+    this.c2 = '#ff00ff'
     this.canvas = canvas
     this.canvas.width = this.w
     this.canvas.height = this.h
-    this.barcodeColorValues = ['#000000', '#ffffff']
+    this.barcodeColorValues = ['#ffffff', '#000000']
     this.ctx = this.canvas.getContext('2d')
+
+    this.number = 0
+    this.options = [
+      ['#ff00ff', '#00ffff', '#ffff00'],
+      ['#0000ff', '#ff0000', '#00ff00']
+    ]
   }
 
-  drawBorder(l) {
+  drawBorder(colors, space) {
+    this.clearAll()
+    this.drawX(colors, space)
+
     let ctx = this.ctx
+
     ctx.beginPath()
-    ctx.fillStyle = this.c1
-    ctx.rect(0, 0, this.w, l)
-    ctx.rect(0, 0, l, this.h / 2)
-    ctx.rect(this.w - l, 0, l, this.h / 2)
+    ctx.fillStyle = colors[1]
+    ctx.rect(0, 0, this.w, this.borderWidth)
+    ctx.rect(0, 0, this.borderWidth, this.h / 2)
+    ctx.rect(this.w - this.borderWidth, 0, this.borderWidth, this.h / 2)
     ctx.fill()
 
     ctx.beginPath()
-    ctx.fillStyle = this.c2
-    ctx.rect(0, this.h - l, this.w, l)
-    ctx.rect(0, this.h / 2, l, this.h / 2)
-    ctx.rect(this.w - l, this.h / 2, l, this.h / 2)
+    ctx.fillStyle = colors[2]
+    ctx.rect(0, this.h - this.borderWidth, this.w, this.borderWidth)
+    ctx.rect(0, this.h / 2, this.borderWidth, this.h / 2)
+    ctx.rect(
+      this.w - this.borderWidth,
+      this.h / 2,
+      this.borderWidth,
+      this.h / 2
+    )
     ctx.fill()
   }
 
-  drawX(l) {
-    let lineWidth = l
+  drawX(colors, space) {
+    let lineWidth = this.borderWidth
     let circleRadius = lineWidth * 1.2
 
     let ctx = this.ctx
-
-    ctx.lineWidth = 2*lineWidth
+    ctx.lineWidth = lineWidth + 2 * space
+    // ctx.beginPath()
+    // ctx.strokeStyle = '#ffffff'
+    // ctx.moveTo(0, 0)
+    // ctx.lineTo(this.w / 2, this.h / 2)
+    // ctx.lineTo(this.w, 0)
+    // ctx.stroke()
+    // ctx.beginPath()
+    // ctx.strokeStyle = '#ffffff'
+    // ctx.moveTo(0, this.h)
+    // ctx.lineTo(this.w / 2, this.h / 2)
+    // ctx.lineTo(this.w, this.h)
+    // ctx.stroke()
+    // ctx.lineWidth = lineWidth
     ctx.beginPath()
-    ctx.strokeStyle = this.white
+    ctx.strokeStyle = colors[1]
     ctx.moveTo(0, 0)
-    ctx.lineTo(this.w / 2, this.h / 2)
+    ctx.lineTo((this.w * 1) / 6, (this.h * 1) / 6)
+    ctx.moveTo((this.w * 2) / 6, (this.h * 2) / 6)
+    ctx.lineTo((this.w * 3) / 6, (this.h * 3) / 6)
+    ctx.lineTo((this.w * 4) / 6, (this.h * 2) / 6)
+    ctx.moveTo((this.w * 5) / 6, (this.h * 1) / 6)
     ctx.lineTo(this.w, 0)
     ctx.stroke()
     ctx.beginPath()
-    ctx.strokeStyle = this.white
+    ctx.strokeStyle = colors[2]
     ctx.moveTo(0, this.h)
-    ctx.lineTo(this.w / 2, this.h / 2)
-    ctx.lineTo(this.w, this.h)
-    ctx.stroke()
-
-    ctx.lineWidth = lineWidth
-    ctx.beginPath()
-    ctx.strokeStyle = this.c1
-    ctx.moveTo(0, 0)
-    ctx.lineTo(this.w / 2, this.h / 2)
-    ctx.lineTo(this.w, 0)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.strokeStyle = this.c2
-    ctx.moveTo(0, this.h)
-    ctx.lineTo(this.w / 2, this.h / 2)
+    ctx.lineTo((this.w * 1) / 6, (this.h * 5) / 6)
+    ctx.moveTo((this.w * 2) / 6, (this.h * 4) / 6)
+    ctx.lineTo((this.w * 3) / 6, (this.h * 3) / 6)
+    ctx.lineTo((this.w * 4) / 6, (this.h * 4) / 6)
+    ctx.moveTo((this.w * 5) / 6, (this.h * 5) / 6)
     ctx.lineTo(this.w, this.h)
     ctx.stroke()
 
     ctx.beginPath()
-    ctx.fillStyle = '#ff0000'
+    ctx.fillStyle = colors[0]
     ctx.arc(this.w / 2, this.h / 2, circleRadius, 0, Math.PI * 2)
     ctx.fill()
+  }
+
+  clearAll() {
+    const ctx = this.ctx
+    ctx.fillStyle = '#ffffff'
+    ctx.fillRect(0, 0, this.w, this.h)
   }
 
   clearBarcode() {
     const ctx = this.ctx
     ctx.fillStyle = '#ffffff'
-    ctx.fillRect(
-      this.borderWidth,
-      this.borderWidth,
-      this.w - 2 * this.borderWidth,
-      this.h - 2 * this.borderWidth
-    )
+    ctx.fillRect(0, 0, this.w, this.h)
+    this.drawBorder(this.options[this.number], 0)
   }
 
   barcode(clientNb, sections) {
     //Only integers are allowed because otherwise the barcode is not complete at the end.
+    sections = Math.round(sections)
+
+    this.drawBorder(this.options[this.number], 0)
+
     const ctx = this.ctx
+
     let code = []
-    let even = false
-    let amount = 0
-    if (clientNb % 2 === 0){
-      even = true
+    let value = clientNb % 2 == 0 ? 0 : 1
+    for (let i = 0; i < Math.floor(clientNb / 2) + 2; i++) {
+      code.push(value)
+      value = (value + 1) % 2
     }
-    if (even) {
-      amount = clientNb / 2 + 2
-    } else {
-      amount = Math.round((clientNb + 2) / 2)
-    }
-    for (let i = 0; i < amount; i++) {
-      if(even){
-        code.push(1)
-      } else{
-        code.push(0)
-      }
-      even = !even
-    }
-    console.log(code)
-    const barAmount = (sections*code.length)+(sections+1)
-    const barWidth = (this.canvas.width - 2 * this.borderWidth) / barAmount
+
+    const codeWidth = this.w - 2 * this.borderWidth
+    const barWidth = codeWidth / (sections * (code.length + 1) + 1)
     let startAt = this.borderWidth
 
     ctx.beginPath()
-    ctx.fillStyle = '#00ffff'
-    ctx.rect(startAt, 0, barWidth, this.h)
+    ctx.fillStyle = '#808080'
+    ctx.rect(startAt, this.borderWidth, barWidth, this.h - 2 * this.borderWidth)
     ctx.fill()
     startAt += barWidth
 
-    //const amountOfIterations = Math.floor((this.w - 2 * this.borderWidth) / (6 * width))
-    for (let i = 0; i < sections * (code.length + 1); i += (code.length + 1)) {
+    for (let i = 0; i < sections; i++) {
       for (let j = 0; j < code.length; j++) {
         ctx.beginPath()
         ctx.fillStyle = this.barcodeColorValues[code[j]]
-        ctx.rect(startAt, 0, barWidth, this.h)
+        ctx.rect(
+          startAt,
+          this.borderWidth,
+          barWidth,
+          this.h - 2 * this.borderWidth
+        )
         ctx.fill()
         startAt += barWidth
-        ctx.beginPath()
       }
       ctx.beginPath()
-      ctx.fillStyle = '#00ffff'
-      ctx.rect(startAt, 0, barWidth, this.h)
+      ctx.fillStyle = '#808080'
+      ctx.rect(
+        startAt,
+        this.borderWidth,
+        barWidth,
+        this.h - 2 * this.borderWidth
+      )
       ctx.fill()
       startAt += barWidth
     }
 
-    this.drawX(this.borderWidth, barWidth)
-    this.drawBorder(this.borderWidth)
+    this.drawX(this.options[this.number], 0)
 
     return barWidth
   }
