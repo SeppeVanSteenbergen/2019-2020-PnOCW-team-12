47512a611926020d2eb8c675f8a45d0bc3bcb773
Unknown <seppevansteenbergen@hotmail.com>
2020-04-24 17:50:56 +0200

    update sensors

--- client/src/algorithms/Sensors.js
+++ client/src/algorithms/Sensors.js
@@ -1,43 +1,47 @@
 export default class Sensors {
   constructor(cssMatrix, callback) {
-    this.originalTransformation = new DOMMatrix(cssMatrix);
-    this.startMatrix = null;
+    this.originalTransformation = new DOMMatrix(cssMatrix)
+    this.startMatrix = null
 
     Promise.all([
       navigator.permissions.query({ name: 'accelerometer' }),
       navigator.permissions.query({ name: 'gyroscope' })
     ]).then(results => {
       if (results.every(result => result.state === 'granted')) {
-        const options = {frequency: 60, coordinateSystem: 'device'};
-        const sensor = new RelativeOrientationSensor(options);
+        const options = { frequency: 60, coordinateSystem: 'device' }
+        this.sensor = new RelativeOrientationSensor(options)
 
-        sensor.addEventListener('reading', () => {
-          if (this.startMatrix === null) setStartMatrix();
+        this.sensor.addEventListener('reading', () => {
+          if (this.startMatrix === null) this.setStartMatrix()
 
-          let rotationMatrix = new DOMMatrix();
-          sensor.populateMatrix(rotationMatrix);
-          rotationMatrix.multiplySelf(this.startMatrix);
-          rotationMatrix.multiplySelf(this.originalTransformation);
-          rotationMatrix.invertSelf();
+          let rotationMatrix = new DOMMatrix()
+          this.sensor.populateMatrix(rotationMatrix)
+          rotationMatrix.multiplySelf(this.startMatrix)
 
-          callback(rotationMatrix);
-        });
-        sensor.addEventListener('error', error => {
+          callback(rotationMatrix)
+        })
+        this.sensor.addEventListener('error', error => {
           if (event.error.name === 'NotReadableError') {
             console.log('Sensor is not available.')
           }
-        });
+        })
 
-        sensor.start()
+        this.sensor.start()
       } else {
         console.log('No permissions to use RelativeOrientationSensor.')
       }
-    });
+    })
+  }
 
-   function setStartMatrix() {
-      let rotationMatrix = new DOMMatrix();
-      sensor.populateMatrix(rotationMatrix);
-      this.startMatrix = rotationMatrix.inverse();
-    }
+  setStartMatrix() {
+    let rotationMatrix = new DOMMatrix()
+    this.sensor.populateMatrix(rotationMatrix)
+    this.startMatrix = rotationMatrix.inverse()
+  }
+
+  static transformationMatrix(originalTransforMation, rotationMatrix) {
+    rotationMatrix.multiplySelf(this.originalTransformation)
+    rotationMatrix.invertSelf()
+    return rotationMatrix
   }
 }
