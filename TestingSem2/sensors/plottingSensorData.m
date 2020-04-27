ec9b64ab801fdc3028489bf1172102b9fb0119b6
martijndebeuf <martijn.debeuf@student.kuleuven.be>
2020-04-27 21:06:48 +0200

    To the moon and back with seppe's accelerometer

--- TestingSem2/sensors/plottingSensorData.m
+++ TestingSem2/sensors/plottingSensorData.m
@@ -4,17 +4,18 @@ clear variables;
 
 sensorData = readtable('sensorData.csv', 'PreserveVariableNames',true);
 
-sensorData.('Velocity x-axis') = cumtrapz(sensorData.Time, sensorData.('Acceleration x-axis'));
-sensorData.('Velocity y-axis') = cumtrapz(sensorData.Time, sensorData.('Acceleration y-axis'));
-sensorData.('Velocity z-axis') = cumtrapz(sensorData.Time, sensorData.('Acceleration z-axis'));
+sensorData.Time = sensorData.Time/1000;
+sensorData.('Velocity x-axis') = cumtrapz(sensorData.Time, sensorData.('Linear acceleration x-axis'));
+sensorData.('Velocity y-axis') = cumtrapz(sensorData.Time, sensorData.('Linear acceleration y-axis'));
+sensorData.('Velocity z-axis') = cumtrapz(sensorData.Time, sensorData.('Linear acceleration z-axis'));
 sensorData.('Position x-axis (in m)') = cumtrapz(sensorData.Time, sensorData.('Velocity x-axis'));
 sensorData.('Position y-axis (in m)') = cumtrapz(sensorData.Time, sensorData.('Velocity y-axis'));
 sensorData.('Position z-axis (in m)') = cumtrapz(sensorData.Time, sensorData.('Velocity z-axis'));
 
-sensorData.Time = milliseconds(sensorData.Time);
+sensorData.Time = seconds(sensorData.Time);
 sensorData = table2timetable(sensorData);
 
-figure('Name', 'OriÃ«ntatie apparaat volgens de x-as, y-as en z-as', 'NumberTitle', 'off')
+figure('Name', 'Oriëntatie apparaat volgens de x-as, y-as en z-as', 'NumberTitle', 'off')
 s = stackedplot(sensorData, {'Orientation x-axis (in degrees)', 'Orientation y-axis (in degrees)', 'Orientation z-axis (in degrees)'});
 s.LineWidth = 2;
 
