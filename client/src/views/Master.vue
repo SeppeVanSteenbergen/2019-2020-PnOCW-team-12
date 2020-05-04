cc19e9aab9171944e8050e5aacdab7a588882c8e
toonsauvillers <toon.sauvillers@gmail.com>
2020-05-04 16:55:03 +0200

    haakske teveel wette wel

--- client/src/views/Master.vue
+++ client/src/views/Master.vue
@@ -1482,7 +1482,6 @@ export default {
           to: 'all'
         }
         this.$socket.emit('screenCommand', object)
-      }
     },
     executeStartTracking() {
       this.executeInitTracking()
