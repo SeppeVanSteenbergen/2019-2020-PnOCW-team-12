8f800e62766e604e72d3e33c2f01fbcd9b4cb955
Unknown <seppevansteenbergen@hotmail.com>
2020-05-05 10:18:05 +0200

    decrease timeout

--- client/src/algorithms/CameraTracking.js
+++ client/src/algorithms/CameraTracking.js
@@ -161,7 +161,7 @@ export default class CameraTracking {
       if (this.isTracking) {
         setTimeout(() => {
           this.run(callback)
-        }, 500)
+        }, 10)
       }
     })
   }
