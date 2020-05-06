2053ba3755e6023df7e6d84eb912e1a86ce645c2
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2020-05-06 12:34:12 +0200

    ssl

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const fs = require('fs')
 const path = require('path')
 const credentials = {
   key: fs.readFileSync((!config.dev && !config.kul)?'/ssl/key.pem':path.join(__dirname, 'keys/private.key'), 'utf8'),
-  cert: fs.readFileSync((!config.dev && !config.kul)?'/ssl/key.pem':path.join(__dirname, 'keys/certificate.crt'), 'utf8')
+  cert: fs.readFileSync((!config.dev && !config.kul)?'/ssl/cert.pem':path.join(__dirname, 'keys/certificate.crt'), 'utf8')
 }
 const httpsServer = https.createServer(credentials, app)
 
