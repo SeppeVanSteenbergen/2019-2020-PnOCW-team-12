ad39503facd55f9cbafb70c72462ecab0534a867
toonsauvillers <toon.sauvillers@gmail.com>
2020-05-12 15:54:20 +0200

    testing ORB finished

--- Testing/ORB_testing/TestORB.js
+++ Testing/ORB_testing/TestORB.js
@@ -4,7 +4,6 @@ function getTranslation(basePicture, picture, width, height, threshold){
 
     let baseCorners = FASTDetector(basePictureGray, width, height, threshold)
     let pictureCorners = FASTDetector(pictureGray, width, height, threshold)
-
     let brief = new Brief(520);
 
     let baseDescriptor = brief.getDescriptors(basePictureGray, width, baseCorners)
@@ -21,16 +20,17 @@ function getTranslation(basePicture, picture, width, height, threshold){
 
     let selectedCount = 0
     for (let i = 0; i < matches.length; i++) {
-        if (matches[i].confidence > 0.75) {
+        if (matches[i].confidence > 0.95) {
             selectedCount++
             trans.x += matches[i].keypoint1[0] - matches[i].keypoint2[0]
             trans.y += matches[i].keypoint1[1] - matches[i].keypoint2[1]
         }
     }
+
     if (selectedCount > 0) {
         trans.x = trans.x / selectedCount
         trans.y = trans.y / selectedCount
     }
 
-    return trans;
+    return {trans: trans, nbUseful: selectedCount, nbKeypoints: baseCorners.length};
 }
